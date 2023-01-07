5b4e1a61a533b5ccff6825401766bd11772f8067
Kwabena Nana Acheampong <achiekobby17@gmail.com>
2023-01-07 07:54:20 +0000

    form ui completed

--- src/components/WorkoutCard.jsx
+++ src/components/WorkoutCard.jsx
@@ -4,6 +4,22 @@ import { RiDeleteBin5Line } from "react-icons/ri";
 import "../scss/WorkoutCard.scss"
 const WorkoutCard = () => {
   return (
+    <div className="workouts">
+<div className="workout-card">
+      <div className="card_container">
+        <div className="card_content">
+          <h3 className="workout_title">Sample title</h3>
+          <div className="workout_specs">
+            <p>Load: <span>20kg</span></p>
+            <p>Reps: <span>20kg</span></p>
+          </div>
+        </div>
+        <div className="actions">
+          <BiEdit className="edit" />
+          <RiDeleteBin5Line className="delete" />
+        </div>
+      </div>
+    </div>
     <div className="workout-card">
       <div className="card_container">
         <div className="card_content">
@@ -19,6 +35,8 @@ const WorkoutCard = () => {
         </div>
       </div>
     </div>
+    </div>
+    
   );
 };
 
--- src/components/WorkoutForm.jsx
+++ src/components/WorkoutForm.jsx
@@ -1,15 +1,51 @@
-import React from 'react'
-import "../scss/WorkoutForm.scss"
+import React from "react";
+import "../scss/WorkoutForm.scss";
 const WorkoutForm = () => {
   return (
-    <div className="form">
+    <div className="form_card">
       <div className="container">
         <div className="wrapper">
-          <h1>Add New Workout Routine</h1>
+          <div className="title">
+            <h1>Add New Workout Routine</h1>
+          </div>
+          <div className="workout_form">
+            <form className="form">
+              <div className="input_group">
+                <label htmlFor="title">Workout Title</label>
+                <input
+                  type="text"
+                  name="title"
+                  id="title"
+                  placeholder="Workout Title..."
+                />
+              </div>
+              <div className="input_group">
+                <label htmlFor="load">Load</label>
+                <input
+                  type="number"
+                  name="load"
+                  id="load"
+                  placeholder="Load in kg..."
+                />
+              </div>
+              <div className="input_group">
+                <label htmlFor="reps">Reps</label>
+                <input
+                  type="number"
+                  name="reps"
+                  id="reps"
+                  placeholder="Reps..."
+                />
+              </div>
+              <button type="submit" className="btn-button">
+                Add Workout
+              </button>
+            </form>
+          </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default WorkoutForm
+export default WorkoutForm;
