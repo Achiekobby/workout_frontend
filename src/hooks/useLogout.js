5332fbc984ef6a626b9ffd98b88a552eae09574f
Kwabena Nana Acheampong <achiekobby17@gmail.com>
2023-01-12 17:07:03 +0000

    clearing the global context for workouts when logout action is triggered

--- src/hooks/useLogout.js
+++ src/hooks/useLogout.js
@@ -1,16 +1,18 @@
+import useAuthContext from "./useAuthContext";
+import { useWorkoutsContext } from "./useWorkoutsContext";
 
-import useAuthContext from './useAuthContext';
 export const useLogout = () => {
-
   const { dispatch } = useAuthContext();
+  const { dispatch: workoutsDispatch } = useWorkoutsContext();
 
   const logout = function () {
     //* remove the user details from the local storage
     localStorage.removeItem("user");
 
     //* dispatch logout function
-    dispatch({type:"LOGOUT"})
+    dispatch({ type: "LOGOUT" });
+    workoutsDispatch({ type: "SET_WORKOUTS", payload: null });
   };
 
-  return {logout}
+  return { logout };
 };
