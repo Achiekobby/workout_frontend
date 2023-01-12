4d65485b97a87a9948be0d96e618b50cb8dcb848
Kwabena Nana Acheampong <achiekobby17@gmail.com>
2023-01-12 16:14:24 +0000

    backend middleware bug fix

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
 //* Pages
 import Home from "./pages/Home";
 import Signup from "./pages/Signup";
-import Login from './pages/Login';
-
-//* components
+import Login from "./pages/Login";
+import useAuthContext from "./hooks/useAuthContext";
 
 function App() {
+  const { user } = useAuthContext();
   return (
     <div className="App">
       <BrowserRouter>
         <div className="pages">
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/signup" element={<Signup/>} />
-            <Route path="/login" element={<Login/>} />
-          
+            <Route
+              path="/"
+              element={user ? <Home /> : <Navigate to="/login" />}
+            />
+            <Route
+              path="/signup"
+              element={!user ? <Signup /> : <Navigate to="/" />}
+            />
+            <Route
+              path="/login"
+              element={!user ? <Login /> : <Navigate to="/" />}
+            />
           </Routes>
         </div>
       </BrowserRouter>
